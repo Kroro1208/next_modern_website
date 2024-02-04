4c1733372c15075d530d06e4134707697227b44a
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-02-04 14:35:50 +0900

    Footerメニュー、各SNSアイコン実装してスタイリング設定した

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,18 @@
 import React from 'react'
 import Container from "./Container"
+import { FaGithubAlt } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa6";
+import { FaYoutube } from "react-icons/fa6";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { TbMinusVertical } from "react-icons/tb";
+import Link from 'next/link';
+
+
+
+
+
+
 
 function Footer() {
     const footerList = [
@@ -30,26 +43,28 @@ function Footer() {
     ]
 
     return (
-        <div>
+        <div className='w-full bg-blueLight mt-10'>
             <Container>
-                <div>
-                    <p>© 2024 Naoya.Y</p>
-                    <ul>
+                <div className='text-sm flex flex-col md:flex-row md:items-center gap-2 justify-between mb-2'>
+                    <p className='mb-2 text-gray-700'>© 2024 Naoya.Y</p>
+                    <ul className='flex items-center gap-2 mb-2'>
                         {
                             footerList.map((item) => (
-                                <li key={item.id}>
-                                    {item.title}
-                                    {item.icon}
-                                </li>
+                                <Link key={item.id} href={item.link}>
+                                    <li className='flex items-center gap-2 text-gray-500 hover:text-primeColor duration-300 cursor-pointer'>
+                                        {item.title}
+                                        {item.icon && <TbMinusVertical />}
+                                    </li>
+                                </Link>
                             ))
                         }
                     </ul>
-                    <div>
-                        <a href="">GitHub</a>
-                        <a href="">facebook</a>
-                        <a href="">Youtube</a>
-                        <a href="">Instagram</a>
-                        <a href="">X(旧Twitter)</a>
+                    <div className='flex items-center gap-4 mx-3 text-gray-700'>
+                        <a href="" target='_blank' className='text-xl hover:text-primeColor cursor-pointer duration-300'><FaGithubAlt /></a>
+                        <a href="" target='_blank' className='text-xl hover:text-primeColor cursor-pointer duration-300'><FaFacebook /></a>
+                        <a href="" target='_blank' className='text-xl hover:text-primeColor cursor-pointer duration-300'><FaYoutube /></a>
+                        <a href="" target='_blank' className='text-xl hover:text-primeColor cursor-pointer duration-300'><FaInstagram /></a>
+                        <a href="" target='_blank' className='text-xl hover:text-primeColor cursor-pointer duration-300'><FaSquareXTwitter /></a>
                     </div>
                 </div>
             </Container>
