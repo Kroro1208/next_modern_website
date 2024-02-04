090666783796196d929615b15debefb53dcdb18b
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-02-04 12:34:54 +0900

    import文修正

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/header";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
